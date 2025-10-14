4d6ca64d66bb048fc03967eb40315e5de2d117ea
beckslange <beckslange96@gmail.com>
2025-10-14 01:36:56 -0600

    added basic js

--- src/index.js
+++ src/index.js
@@ -14,9 +14,12 @@ function generatePoem(event) {
   let poemElement took its place, then new Typewriter was added*/
 }
 /*function generatePoem was the second part of JS that was created
-now when you type in a prompt like "flowers into the search bar, the
+now when you type in a prompt like "flowers" into the search bar, the
 alert "Generating poem" pops up
 after doing this we want to go back and add an id "poem" to our div*/
+/*new Typewriter was the third element added*/
+/*This was the basic js that added the typewriter element. Now we 
+  are going to add the API element to make it look even better*/
 
 let poemFormElement = document.querySelector("#poem-generator-form");
 poemFormElement.addEventListener("submit", generatePoem);
